732ed4a64f99de2b4210fda04acb8a22121629ea
Johan Ekenberg <johan.ekenberg@gmail.com>
2013-11-30 14:31:21 +0100

    Prompt before kill. Error handling.

--- kill-buffer-other-window.el
+++ kill-buffer-other-window.el
@@ -1,9 +1,14 @@
 (defun kill-buffer-other-window ()
   "Kill the buffer in the next (clockwise) window without leaving the current window"
   (interactive)
-  (progn
-    (other-window 1)
-    (kill-buffer)
-    (other-window -1)))
+  (cond ((< (count-windows) 2)
+         (message "There is no other window to kill")
+         )
+        (t
+            (other-window 1)
+            (if (y-or-n-p (format "Kill buffer %s?" (buffer-name)))
+                (kill-buffer))
+            (other-window -1)
+            (message ""))))
 
 (provide 'kill-buffer-other-window)
