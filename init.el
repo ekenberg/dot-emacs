2a4a10725a8aac532babf3ace2e3b3d54c2acc94
Johan Ekenberg <johan.ekenberg@gmail.com>
2013-12-12 22:48:37 +0100

    Improve comment format

--- init.el
+++ init.el
@@ -1,24 +1,24 @@
-; --------------------------------------------------------------------------------
-; Combined Emacs init-file for linux and mac
-; Used with Swedish keyboard layout
-; --------------------------------------------------------------------------------
+;;;; --------------------------------------------------------------------------------
+;;;; Combined Emacs init-file for linux and mac
+;;;; Used with Swedish keyboard layout
+;;;; --------------------------------------------------------------------------------
 
-; Load customizations from subdir
+;; Load customizations from subdir
 (add-to-list 'load-path "~/.emacs.d/")
 
-; Use Melpa for extra packages
+;; Use Melpa for extra packages
 (require 'package)
 (add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/") t)
 
 (package-initialize)
 
-; Custom themes folder
+;; Custom themes folder
 (add-to-list 'custom-theme-load-path "~/.emacs.d/themes")
 (load-theme 'my-dev-1 t)
 
-; --------------------------------------------------------------------------------
-; Customizations for GUI emacs, ie GTK or Cocoa
-; --------------------------------------------------------------------------------
+;;; --------------------------------------------------------------------------------
+;;; Customizations for GUI emacs, ie GTK or Cocoa
+;;; --------------------------------------------------------------------------------
 
 (if window-system
     (progn
@@ -50,24 +50,24 @@
   )
 
 
-; --------------------------------------------------------------------------------
-; Load custom functions + keyboard shortcuts
-; --------------------------------------------------------------------------------
+;;; --------------------------------------------------------------------------------
+;;; Load custom functions + keyboard shortcuts
+;;; --------------------------------------------------------------------------------
 
-; Swap content between two windows
+;; Swap content between two windows
 (load-library "swap-windows")
 (global-set-key (kbd "\C-x 6") 'swap-windows)
 
-; Toggle vertical/horizontal split of two windows
+;; Toggle vertical/horizontal split of two windows
 (load-library "rotate-frame-split")
 (global-set-key (kbd "\C-x 5") 'rotate-frame-split)
 
-; Save copy of buffer without switching to the new buffer/file name
+;; Save copy of buffer without switching to the new buffer/file name
 (load-library "save-copy-as")
 (global-set-key "\C-c\C-w" 'save-copy-as)
 
-; dabbrev-expand via C-TAB
-; TODO: This doesn't work in a terminal
+;; dabbrev-expand via C-TAB
+;; TODO: This doesn't work in a terminal
 (global-set-key (kbd "C-<tab>") 'dabbrev-expand)
 (define-key minibuffer-local-map (kbd "C-<tab>") 'dabbrev-expand)
 
@@ -87,44 +87,44 @@
 (global-set-key (kbd "\C-x |") 'smart-shell-command)
 
 
-; --------------------------------------------------------------------------------
-; Other keyboard fixes and shortcuts
-; --------------------------------------------------------------------------------
+;;; --------------------------------------------------------------------------------
+;;; Other keyboard fixes and shortcuts
+;;; --------------------------------------------------------------------------------
 
-; backspace/C-h fix:
+;; backspace/C-h fix:
 (global-set-key "\C-h" 'backward-delete-char)
 (normal-erase-is-backspace-mode 0)
 
-; Help key:
+;; Help key:
 (global-set-key (kbd "C-+") 'help)
 
-; Mac, Swedish keyboard: Leave right alt-key alone, else I cannot write \
-; Solution found at the bottom of this page (French keyboard): http://stackoverflow.com/questions/6344389/osx-emacs-unbind-just-the-right-alt
+;; Mac, Swedish keyboard: Leave right alt-key alone, else I cannot write \
+;; Solution found at the bottom of this page (French keyboard): http://stackoverflow.com/questions/6344389/osx-emacs-unbind-just-the-right-alt
 (setq-default mac-option-modifier nil)
 
 
-; --------------------------------------------------------------------------------
-; Options mainly for programming
-; --------------------------------------------------------------------------------
+;;; --------------------------------------------------------------------------------
+;;; Options mainly for programming
+;;; --------------------------------------------------------------------------------
 
-; C-mode
+;; C-mode
 (setq c-default-style "linux"
       c-basic-offset 4)
 
-; Indent with spaces, not tabs
+;; Indent with spaces, not tabs
 (setq-default indent-tabs-mode nil)
 
-; http://www.gnu.org/software/emacs/manual/html_node/emacs/Hungry-Delete.html
+;; http://www.gnu.org/software/emacs/manual/html_node/emacs/Hungry-Delete.html
 (setq c-toggle-hungry-state t)
 
-; Always delete trailing whitespace when saving
+;; Always delete trailing whitespace when saving
 (add-hook 'before-save-hook 'delete-trailing-whitespace)
 
-; Delete trailing whitespace only in C-mode
-;(add-hook 'c-mode-common-hook (lambda () (add-to-list 'write-file-functions 'delete-trailing-whitespace)))
+;; Delete trailing whitespace only in C-mode
+;;(add-hook 'c-mode-common-hook (lambda () (add-to-list 'write-file-functions 'delete-trailing-whitespace)))
 
-; (Un-)Comment key bindings
-; TODO: Improve to custom 'comment-region-or-line, this doesn't quite work ie for perl-mode:
+;; (Un-)Comment key bindings
+;; TODO: Improve to custom 'comment-region-or-line, this doesn't quite work ie for perl-mode:
 (global-set-key "\C-cc" 'comment-region)
 (global-set-key "\C-cu" 'uncomment-region)
 
@@ -188,7 +188,8 @@
 (defun markdown-custom ()
   "my-markdown-mode-hook"
   (setq markdown-open-command "~/local/bin/marked")
-  (setq markdown-command      "~/local/bin/gfm"))  ;; gfm = git-flavoured-markdown; http://github.com/alampros/Docter
+  ;; gfm = git-flavoured-markdown; http://github.com/alampros/Docter
+  (setq markdown-command      "~/local/bin/gfm"))
 (add-hook 'markdown-mode-hook 'markdown-custom)
 
 ;; SLIME
@@ -201,32 +202,37 @@
 (add-to-list 'smart-tab-completion-functions-alist '(slime-repl-mode . slime-complete-symbol))
 (add-hook 'slime-repl-mode-hook (lambda () (run-hooks 'prog-mode-hook))) ; treat as a prog-mode
 
-; Lua-mode:
+
+;;; --------------------------------------------------------------------------------
+;;; Extra modes
+;;; --------------------------------------------------------------------------------
+
+;; Lua-mode:
 (autoload 'lua-mode "lua-mode" "Lua editing mode." t)
 (add-to-list 'auto-mode-alist '("\\.lua$" . lua-mode))
 (add-to-list 'interpreter-mode-alist '("lua" . lua-mode))
 
 
-; --------------------------------------------------------------------------------
-; Various file options
-; --------------------------------------------------------------------------------
+;;; --------------------------------------------------------------------------------
+;;; Various file options
+;;; --------------------------------------------------------------------------------
 
-; Preserve backup-file user/group, especially important under sudo / su
+;; Preserve backup-file user/group, especially important under sudo / su
 (setq backup-by-copying-when-mismatch t)
 
-; Always end files with a newline
+;; Always end files with a newline
 (setq require-final-newline 't)
 
 
-; --------------------------------------------------------------------------------
-; Various other options
-; --------------------------------------------------------------------------------
+;;; --------------------------------------------------------------------------------
+;;; Various other options
+;;; --------------------------------------------------------------------------------
 
-; Disable all Version Control backends. Quicker start-up. Avoid hanging in git-enabled dirs with emacs-gtk on Linux
+;; Disable all Version Control backends. Quicker start-up. Avoid hanging in git-enabled dirs with emacs-gtk on Linux
 (setq vc-handled-backends ())
 
-; No welcome message
+;; No welcome message
 (setq inhibit-startup-message t)
 
 
-; --------------------------------------------------------------------------------
+;;; --------------------------------------------------------------------------------
