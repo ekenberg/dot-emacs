c90af85bd32326168922dafc58a0d6fc48ce0c00
Johan Ekenberg <johan.ekenberg@gmail.com>
2013-12-13 00:36:20 +0100

    Add installer

--- README.md
+++ README.md
@@ -1,9 +1,29 @@
 dot-emacs
 =========
 
-My Emacs customizations
+## My Emacs customizations
 
 * I use a Swedish Keyboard
 * I use Emacs on Mac OS X and Linux (Mint KDE)
 * I use Emacs both in the terminal (no x) and with a gui (Cocoa / GTK)
 * I use this config for all OS:es and Emacs-variants. One config to rule them all.
+
+## Install:
+
+### Automatic:
+The commandline program ```unzip``` is required for installation. It's present by default on most Macs and Linuxes
+
+    curl https://raw.github.com/ekenberg/dot-emacs/master/init.el | sh
+
+or
+
+    wget -qO - https://raw.github.com/ekenberg/dot-emacs/master/init.el | sh
+
+### Manual:
+
+Clone this repo from github and symlink the directory:
+
+    cd
+    # backup old .emacs as/if needed:
+    mv .emacs.d .emacs.d.old
+    ln -s path-to-dot-emacs .emacs.d
---
+++ installer.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+
+function cmd_exists {
+    $1 &> /dev/null && return 0
+    return 1
+}
+
+function die {
+    echo "$1" 1>&2
+    exit 1
+}
+
+[ -z "$HOME" ] && die "Can't find your HOME (\$HOME is empty!?)"
+
+# check for unzip
+cmd_exists unzip || die "No such program"
+
+# check for curl or wget, then get the zip
+TARGET_ZIP="$HOME/dot-emacs-master.zip"
+
+DL_PROG=""
+if cmd_exists "curl -h"; then
+    DL_PROG="curl -L -s -o $TARGET_ZIP"
+elif cmd_exists "wget -h"; then
+    DL_PROG="wget -q -O $TARGET_ZIP"
+else
+    die "You need curl or wget!"
+fi
+
+cd $HOME || die "Can't go \$HOME"
+
+EMACS_D_BAK=$HOME/.emacs.d.orig.bak
+
+echo "Downloading dot-emacs from github..."
+$DL_PROG https://github.com/ekenberg/dot-emacs/archive/master.zip
+
+# (re)move existing .emacs.d
+[ -h $HOME/.emacs.d ] && rm -f $HOME/.emacs.d
+[ -e $HOME/.emacs.d ] && (echo "Backing up your current .emacs.d:"; mv -Tbv $HOME/.emacs.d $EMACS_D_BAK)
+
+TARGET_DIR=$HOME/.emacs.d.dot-emacs
+
+# (re)create $TARGET_DIR
+rm -rf $TARGET_DIR
+mkdir $TARGET_DIR || die
+
+# unpack zip
+cd $TARGET_DIR || die
+unzip -q $TARGET_ZIP || die
+mv dot-emacs-master/* ./ || die
+rmdir dot-emacs-master
+rm -f $TARGET_ZIP
+
+# symlink .emacs.d
+cd $HOME
+ln -s $TARGET_DIR $HOME/.emacs.d
+
+# greet
+echo "dot-emacs is installed and ready to go. Start emacs now."
