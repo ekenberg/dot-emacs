8d35bea7fcf0d9da781900febc0e36134878b459
Johan Ekenberg <johan.ekenberg@gmail.com>
2017-12-06 22:21:22 +0100

    quit-popup-window - handle more modes!

--- lisp/quit-popup-window.el
+++ lisp/quit-popup-window.el
@@ -5,10 +5,15 @@
   (if (and (eq (count-windows) 2)
            (member (with-current-buffer (window-buffer (next-window)) major-mode)
                    '(help-mode
+                     Info-mode
                      debugger-mode
                      compilation-mode
                      grep-mode
-                     Buffer-menu-mode)))
+                     Buffer-menu-mode
+                     apropos-mode
+                     ivy-occur-mode
+                     ivy-occur-grep-mode
+                     )))
       (quit-restore-window (next-window))
 
     (message "Cannot find a popup-window to quit")))
