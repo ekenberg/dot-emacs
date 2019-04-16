4eb3866d3ca1948233d629bb167707befd81ede4
Johan Ekenberg <johan.ekenberg@gmail.com>
2019-04-16 08:38:32 +0200

    Prettify code formatting

--- lisp/smart-shell-command.el
+++ lisp/smart-shell-command.el
@@ -1,10 +1,15 @@
 (defun smart-shell-command-prompt (prompt-prefix)
-  (first (list (read-from-minibuffer prompt-prefix nil nil nil 'shell-command-history) current-prefix-arg))
-  )
+  (first (list
+          (read-from-minibuffer
+           prompt-prefix nil nil nil 'shell-command-history)
+          current-prefix-arg)))
 
 
 (defun smart-shell-command (arg)
-  "No region selected: shell-command arg, output to mini-buffer. Argument passed (C-u) and no region selected: shell-command arg, output at point. Region selected: shell-command-on-region, output to mini-buffer. Argument passed (C-u) and region selected: shell-command-on-region, output at point."
+  "No region selected: shell-command arg, output to mini-buffer. \
+Argument passed (C-u) and no region selected: shell-command arg, output at point. \
+Region selected: shell-command-on-region, output to mini-buffer. \
+Argument passed (C-u) and region selected: shell-command-on-region, output at point."
   (interactive "P")
   (let ((p (if mark-active (region-beginning) 0))
         (m (if mark-active (region-end) 0)))
