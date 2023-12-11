cda794606bb08f5aca55140bb37769ba83869975
Johan Ekenberg <johan.ekenberg@gmail.com>
2023-12-12 00:11:15 +0100

    speed up search/replace with fundamental-mode, make indent optional

--- lisp/mps-cleanup-php.el
+++ lisp/mps-cleanup-php.el
@@ -1,44 +1,60 @@
-(defun mps-cleanup-php ()
-  "Replace quotes, format if/while/for/else etc, indent code"
-  (interactive)
+(defun mps-cleanup-php (&optional indent)
+  "Replace quotes \\\" -> ', format if/while/for/else constructs,
+code-indent according to major mode if INDENT is given"
+  (interactive "P")
   (save-restriction
-    ;; replace:
-    ;; if(...)  =>
-    ;; if (...)
-    ;; and so on with while/for/foreach
-    (goto-char (point-min))
-    (while (re-search-forward "\\(if\\|while\\|for\\|foreach\\)(" nil t)
-      (replace-match "\\1 ("))
+    (let ((orig-mode major-mode))
+      (unwind-protect
+          (progn
+            (fundamental-mode)
 
-    ;; replace:
-    ;; (...){   =>
-    ;; (...) {
-    (goto-char (point-min))
-    (while (re-search-forward "){" nil t)
-      (replace-match ") {"))
+            ;; replace:
+            ;; if(...)  =>
+            ;; if (...)
+            ;; and so on with while/for/foreach
+            (message "Fixing if/while/for/foreach")
+            (goto-char (point-min))
+            (while (re-search-forward "\\(if\\|while\\|for\\|foreach\\)(" nil t)
+              (replace-match "\\1 ("))
 
-    ;; replace:
-    ;; }else{   =>
-    ;; }
-    ;; else {
-    (goto-char (point-min))
-    (while (re-search-forward "}[:blank:]*else[:blank:]*{" nil t)
-      (replace-match "}\nelse {"))
+            ;; replace:
+            ;; (...){   =>
+            ;; (...) {
+            (message "Fixing ){")
+            (goto-char (point-min))
+            (while (search-forward "){" nil t)
+              (replace-match ") {"))
 
-    ;; replace:
-    ;; \"   =>
-    ;; '
-    (goto-char (point-min))
-    (while (re-search-forward "\\\\\"" nil t)
-      (replace-match "'"))
+            ;; replace:
+            ;; }else{   =>
+            ;; }
+            ;; else {
+            (message "Fixing }else{")
+            (goto-char (point-min))
+            (while (re-search-forward "}[:blank:]*else[:blank:]*{" nil t)
+              (replace-match "}\nelse {"))
 
-    ;; indent buffer
-    (indent-region (point-min) (point-max))
+            ;; replace:
+            ;; \"   =>
+            ;; '
+            (message "Fixing quotes \\\" -> '")
+            (goto-char (point-min))
+            (while (search-forward "\\\"" nil t)
+              (replace-match "'"))
 
-    ;; swipe to onload/javascript which will probably need manual fixing of quotes
-    (goto-char (point-min))
-    (when (re-search-forward "onload\\|javascript")
-      (add-to-history 'query-replace-defaults (cons "\\\"" "'"))
-      (swiper "onload\\|javascript"))
+            ;; search-and-replace is done, restore original major mode
+            (funcall orig-mode)
 
-    ))
+            ;; indent buffer
+            (when indent
+              (message "Indenting buffer")
+              (indent-region (point-min) (point-max)))
+
+            ;; swipe to onload/javascript which will probably need manual fixing of quotes
+            (goto-char (point-min))
+            (when (re-search-forward "onload\\|javascript")
+              (add-to-history 'query-replace-defaults (cons "\\\"" "'"))
+              (swiper "onload\\|javascript"))
+
+            )
+        (or (equal major-mode orig-mode) (funcall orig-mode))))))
