b7d8c08e8354ecf4b8062b564d9f1a68527093b3
Johan Ekenberg <johan.ekenberg@gmail.com>
2024-01-16 01:20:43 +0100

    idle-buffer-mode med poll timer

--- lisp/idle-buffer-mode.el
+++ lisp/idle-buffer-mode.el
@@ -1,11 +1,14 @@
-;; The following variables are bound to ALWAYS be local to the current buffer (se docs)
-;;(make-variable-buffer-local 'idle-buffer-active-timers)  ; to cancel or restart all when needed
-(defvar-local idle-buffer-auto-save-sec    nil "How many seconds idle buffer before saving")
-(defvar-local idle-buffer-auto-close-sec   nil "How many seconds idle buffer before closing")
-(defvar-local idle-buffer--save-idle-timer nil "For internal use only")
-(defvar-local idle-buffer--save-timer      nil "For internal use only")
-(defvar-local idle-buffer--close-timer     nil "For internal use only")
-(defvar-local idle-buffer--my-buffer       nil "For internal use only")
+
+;; buffer-local variables:
+(defvar-local idle-buffer-auto-save-sec     0   "How many seconds idle buffer before saving, 0 means disabled")
+(defvar-local idle-buffer-auto-close-sec    0   "How many seconds idle buffer before closing, 0 means disabled")
+(defvar-local idle-buffer--save-idle-timer  nil "For internal use only")
+(defvar-local idle-buffer--close-idle-timer nil "For internal use only")
+(defvar-local idle-buffer--save-timer       nil "For internal use only")
+(defvar-local idle-buffer--close-timer      nil "For internal use only")
+(defvar-local idle-buffer--poll-timer       nil "For internal use only")
+
+(defconst idle-buffer-poll-sec 1 "Poll interval for checking buffer focus and timers")
 
 (define-minor-mode idle-buffer-mode
   "Perform automatic actions on idle buffers"
@@ -15,7 +18,6 @@
   (if idle-buffer-mode
       ;; mode toggled ON
       (progn
-        (setq idle-buffer--my-buffer (current-buffer))
 
         (when (> idle-buffer-auto-save-sec 0)
           (message "idle-buffer-mode: %s activating idle auto save after %d sec" (buffer-name) idle-buffer-auto-save-sec)
@@ -25,89 +27,62 @@
           (message "idle-buffer-mode: %s activating idle auto close after %d sec" (buffer-name) idle-buffer-auto-close-sec)
           (idle-buffer--start-close-idle-timer (current-buffer) idle-buffer-auto-close-sec))
 
+        (when (or (> idle-buffer-auto-save-sec 0)
+                  (> idle-buffer-auto-close-sec 0))
+          (message "idle-buffer-mode: starting poll-timer with interval %d sec" idle-buffer-poll-sec)
+          (idle-buffer--start-poll-timer (current-buffer) idle-buffer-poll-sec))
+
         (add-hook 'kill-buffer-hook #'idle-buffer--kill-buffer-handler)
-        (add-hook 'buffer-list-update-hook #'idle-buffer--buffer-list-update-handler))
+
+        )
 
     ;; mode toggled OFF
+    (idle-buffer--cancel-all-timers)
     (remove-hook 'kill-buffer-hook #'idle-buffer--kill-buffer-handler)
-    (remove-hook 'buffer-list-update-hook #'idle-buffer--buffer-list-update-handler)))
-
-;; Ok problemet är att när man triggar buffer-list-update-hook så vet man inte
-;; vilken buffer man lämnar och vilken man anländer till
-;; Beroende på vilken funktion som triggar hooken så får man från-buffern eller till-buffern i (current-buffer)
-;; Men om man väntar en sekund (run-with-timer) så får man konsekvent till-buffern i (current-buffer)
-;; Problemet är då att om man lämnat "vår" buffer eller växlar mellan två andra buffers, så
-;; är inte de buffer-lokalt definierade variablerna tillgängliga för att sätta timers (när man lämnar "vår" buffer)
-;;
-;; Skit i lambdas eller konstiga defalias-konstruktioner för nu
-;;
-;; Vad om vi när hooken körs utan argument direkt kollar (current-buffer) samt idle-buffer--my-buffer
-;; sen skickar vi med dem båda i run-with-timer
-;; och när vi kör igen så kan vi hämta nya (current-buffer)
-;; Nu vet vi
-;;   - vilken buffer som var aktiv när hooken anropades
-;;   - vilken vi slutligen landade i
-;;   - om idle-buffer--my-buffer var satt från början
-;;   - om idle-buffer--my-buffer är satt när vi landat
-;;
-;; med hjälp av dessa värden kan vi (väl) lista ut läget?
-;; Exempel:
-;; (defun handler (&optional hook-buffer hook-my-buffer)
-;;   (when (and hook-buffer hook-my-buffer)
-;;     (let ((final-buffer (current-buffer))
-;;           (final-my-buffer (and (boundp idle-buffer--my-buffer) idle-buffer--my-buffer)))
-;;       ;; case växlar mellan två andra buffers
-;;       ;; case växlar från "vår" buffer till annan
-;;       ;; case växlar från annan buffer till "vår"
-;;       ;; case växlar mellan "vår" buffer och en annan som också har idle-buffer-mode
-
-;;       ;; kan man räkna med att om hook-buffer == final-buffer så är vi i TILL-buffern och vi vet inte FRÅN-buffern
-;;       ;;    det betyder i så fall att växling mellan två idle-buffer-mode
-;;       ;; om de inte är samma
-
-;;       )
-;;     ))
-
-
-(defun idle-buffer--buffer-list-update-handler (&optional really-run)
-  (message "id dbg in update handler (%s)" really-run)
-  (if really-run
-      (progn
-        ;; back in our buffer, cancel timers
-        (if (eq idle-buffer--my-buffer (current-buffer))
-            (progn
-              (message "id dbg BACK IN OUR BUFFER")
-              (idle-buffer--cancel-save-timer)
-              (idle-buffer--cancel-close-timer))
-          ;; leaving our buffer, start timers as needed
-          (message "id dbg OUTSIDE OUR BUFFER")
-          (when (and (> idle-buffer-auto-save-sec 0)
-                     (not idle-buffer--save-timer))
-            (setq idle-buffer--save-timer (run-with-timer idle-buffer-auto-save-sec nil 'idle-buffer--save idle-buffer--my-buffer)))
-          (when (and (> idle-buffer-auto-close-sec 0)
-                     (not idle-buffer--close-timer))
-            (setq idle-buffer--close-timer (run-with-timer idle-buffer-auto-close-sec nil 'idle-buffer--close idle-buffer--my-buffer)))))
-    ;; else run this function after one second wait, to get correct current buffer
-    (run-with-timer 1 nil 'idle-buffer--buffer-list-update-handler t)))
+    ))
 
 (defun idle-buffer--kill-buffer-handler ()
   (idle-buffer--cancel-all-timers))
 
 (defun idle-buffer--cancel-all-timers ()
+  (idle-buffer--cancel-poll-timer)
   (idle-buffer--cancel-save-idle-timer)
+  (idle-buffer--cancel-close-idle-timer)
   (idle-buffer--cancel-save-timer)
-  (idle-buffer--cancel-close-timer))
+  (idle-buffer--cancel-close-timer)
+  )
 
-(defun idle-buffer--start-close-idle-timer (buf sec)
-  (when (and (boundp 'sec) (integerp sec) (> sec 0))
-    (run-with-idle-timer sec nil 'idle-buffer--close buf))) ; no repeat for close timer
+(defun idle-buffer--start-poll-timer (my-buffer interv)
+  (setq idle-buffer--poll-timer (run-with-timer interv interv 'idle-buffer--run-poll my-buffer)))
+
+(defun idle-buffer--start-save-timer (buf sec)
+  (with-current-buffer buf
+    (when (and (null idle-buffer--save-timer) (boundp 'sec) (integerp sec) (> sec 0))
+      (setq idle-buffer--save-timer (run-with-timer sec sec #'idle-buffer--save buf)))))
+
+(defun idle-buffer--start-close-timer (buf sec)
+  (with-current-buffer buf
+    (when (and (null idle-buffer--close-timer) (boundp 'sec) (integerp sec) (> sec 0))
+      (setq idle-buffer--close-timer (run-with-timer sec nil #'idle-buffer--close buf)))))
 
 (defun idle-buffer--start-save-idle-timer (buf sec)
-  (when (and (boundp 'sec) (integerp sec) (> sec 0))
-    (setq idle-buffer--save-idle-timer (run-with-idle-timer sec t 'idle-buffer--save buf))))
+  (when (and (null idle-buffer--save-idle-timer) (boundp 'sec) (integerp sec) (> sec 0))
+    (setq idle-buffer--save-idle-timer (run-with-idle-timer sec t #'idle-buffer--save buf))))
+
+(defun idle-buffer--start-close-idle-timer (buf sec)
+  (when (and (null idle-buffer--close-idle-timer) (boundp 'sec) (integerp sec) (> sec 0))
+    (setq idle-buffer--close-idle-timer (run-with-idle-timer sec nil #'idle-buffer--close buf)))) ; no repeat for close timer
+
+(defun idle-buffer--run-poll (my-buffer)
+  (if (eq my-buffer (current-buffer))
+      (progn
+        (idle-buffer--cancel-save-timer)
+        (idle-buffer--cancel-close-timer))
+    ;; else
+    (idle-buffer--start-save-timer my-buffer (buffer-local-value 'idle-buffer-auto-save-sec my-buffer))
+    (idle-buffer--start-close-timer my-buffer (buffer-local-value 'idle-buffer-auto-close-sec my-buffer))))
 
 (defun idle-buffer--close (buf)
-  (idle-buffer--cancel-close-timer)
   (when (buffer-live-p buf)
     (with-current-buffer buf
       (when (bound-and-true-p idle-buffer-mode)
@@ -123,10 +98,20 @@
       (when (and (bound-and-true-p idle-buffer-mode) (buffer-modified-p buf))
         (save-buffer)))))
 
+(defun idle-buffer--cancel-poll-timer()
+  (when idle-buffer--poll-timer
+    (cancel-timer idle-buffer--poll-timer)
+    (setq idle-buffer--poll-timer nil)))
+
 (defun idle-buffer--cancel-save-idle-timer()
   (when idle-buffer--save-idle-timer
     (cancel-timer idle-buffer--save-idle-timer)
     (setq idle-buffer--save-idle-timer nil)))
+
+(defun idle-buffer--cancel-close-idle-timer()
+  (when idle-buffer--close-idle-timer
+    (cancel-timer idle-buffer--close-idle-timer)
+    (setq idle-buffer--close-idle-timer nil)))
 
 (defun idle-buffer--cancel-save-timer()
   (when idle-buffer--save-timer
@@ -138,15 +123,5 @@
     (cancel-timer idle-buffer--close-timer)
     (setq idle-buffer--close-timer nil)))
 
-;; TODO
-;;  - starta vanlig timer när fokus lämnar buffern, och cancel när fokus kommer tillbaka
-;;    även om man är aktiv i andra buffers skall det räknas inaktivitet i lokal buffer
-;;    timers skall utföra samma handlingar som de idle-timers som startas
-;;  - försök återskapa 100% cpu med annat än org-mode buffers
-;;  - använd fil-lokal variabel för att aktivera idle-buffer-mode
-;;
-;; BUGS
-;; [2024-01-15 01:51:57.493 CET] closing buffer freewriting.org.gpg after being idle for 10 sec
-;; [2024-01-15 01:51:57.494 CET] Error running timer ‘idle-buffer--close’: (wrong-type-argument timerp nil)
 
 (provide 'idle-buffer-mode)
