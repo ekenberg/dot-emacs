77848bf94602d84ad0d827b397863befb882c283
Johan Ekenberg <johan.ekenberg@gmail.com>
2024-02-02 20:47:58 +0100

    smart-shell-command: Use C-u C-u to COPY output to kill-ring

--- lisp/smart-shell-command.el
+++ lisp/smart-shell-command.el
@@ -1,15 +1,15 @@
 (defun smart-shell-command-prompt (prompt-prefix)
-  (first (list
+  (car (list
           (read-from-minibuffer
            prompt-prefix nil nil nil 'shell-command-history)
           current-prefix-arg)))
 
 
 (defun smart-shell-command (arg)
-  "No region selected: shell-command arg, output to mini-buffer. \
-Argument passed (C-u) and no region selected: shell-command arg, output at point. \
-Region selected: shell-command-on-region, output to mini-buffer. \
-Argument passed (C-u) and region selected: shell-command-on-region, output at point."
+  "No region selected: shell-command arg, output to mini-buffer.
+Region selected: shell-command-on-region, output to mini-buffer.
+Argument passed (C-u): output at point.
+Double argument passed (C-u C-u): copy output to kill ring"
   (interactive "P")
   (let ((p (if mark-active (region-beginning) 0))
         (m (if mark-active (region-end) 0)))
@@ -17,9 +17,23 @@ Argument passed (C-u) and region selected: shell-command-on-region, output at po
         ;; no active region
         (if (eq arg nil)
             (shell-command (smart-shell-command-prompt "Shell command: "))
-          (shell-command (smart-shell-command-prompt "Shell command (insert): ") t))
+          (if (> (prefix-numeric-value arg) 4)
+              ;; C-u C-u:
+              (progn
+                (shell-command (smart-shell-command-prompt "Shell command (copy output): "))
+                (with-current-buffer shell-command-buffer-name
+                  (clipboard-kill-ring-save (point-min) (point-max) )))
+            ;; C-u:
+            (shell-command (smart-shell-command-prompt "Shell command (insert): ") t)))
 
       ;; active region
       (if (eq arg nil)
           (shell-command-on-region p m (smart-shell-command-prompt "Shell command (region): "))
-        (shell-command-on-region p m (smart-shell-command-prompt "Shell command (region replace): ") t t)))))
+        (if (> (prefix-numeric-value arg) 4)
+            ;; C-u C-u:
+            (progn
+              (shell-command-on-region p m (smart-shell-command-prompt "Shell command (region, copy output): "))
+              (with-current-buffer shell-command-buffer-name
+                (clipboard-kill-ring-save (point-min) (point-max))))
+          ;; C-u:
+          (shell-command-on-region p m (smart-shell-command-prompt "Shell command (region replace): ") t t))))))
